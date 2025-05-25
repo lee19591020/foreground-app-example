c164c4d2aa4d51220b2dcd2f34483ddae1dff5f0
Leonil Elvira <leonil.elvira@fusion5.com.au>
2025-05-25 15:03:36 +0800

    fix

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.mycompany.mytabsapp',
+  appId: 'io.ionic.starter',
   appName: 'myTabsApp',
   webDir: 'www'
 };
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,10 @@
 import { Component, NgZone, OnInit } from '@angular/core';
+import { Platform } from '@ionic/angular';
 import {
   CapacitorForegroundLocationService,
   ForegroundLocation,
-  PermissionResponse
+  ForegroundLocationConfiguration,
+  PermissionResponse,
 } from 'capacitor-foreground-location-service';
 
 @Component({
@@ -15,36 +17,80 @@ export class Tab1Page implements OnInit {
   lat = 0;
   lng = 0;
   message = 'updating location..';
-  constructor(private ngZone: NgZone) {}
+  constructor(private ngZone: NgZone, private flatform: Platform) {}
   ngOnInit(): void {
-    this.init();
+    console.log(this.flatform.is('android'));
+    if (this.flatform.is('android')) {
+      this.init();
+    }
+    if (this.flatform.is('ios')) {
+      this.initIOS();
+    }
+  }
+
+  async ionViewDidEnter() {
+    console.log('enter now');
   }
 
   async init() {
     //request permission here using capacitor
-    console.log("NABUANG NAH!");
-    const response = await CapacitorForegroundLocationService.requestPermission()
-      .catch((err: any) => {
-        console.log(`Error man ue: ` + err);
-        return {
-          granted: false
-        } as PermissionResponse;
-      });
-      console.log(response);
-    if(response.granted){
+    const config: ForegroundLocationConfiguration = {
+      distanceFilter: 20,
+      interval: 5000,
+      notificationMessage: 'Something is tracking your location',
+      notificationTitle: 'I am a tracker',
+    };
+    await CapacitorForegroundLocationService.config(config);
+    const response =
+      await CapacitorForegroundLocationService.requestPermission().catch(
+        (err: any) => {
+          console.log(`Error man ue: ` + err);
+          return {
+            granted: false,
+          } as PermissionResponse;
+        }
+      );
+    console.log(response);
+    if (response.granted) {
       CapacitorForegroundLocationService.startService();
       CapacitorForegroundLocationService.addListener(
-      'locationUpdate',
-      (location: ForegroundLocation) => {
-        console.log('[JS] Lat:', location.lat, 'Lng:', location.lng);
-        this.ngZone.run(() => {
-          this.lat = location.lat;
-          this.lng = location.lng;
-          this.message = `Location updated: ${new Date().getTime()}`;
-        });
-      }
-    );
+        'locationUpdate',
+        (location: ForegroundLocation) => {
+          console.log('[JS] Lat:', location.lat, 'Lng:', location.lng);
+          this.ngZone.run(() => {
+            this.lat = location.lat;
+            this.lng = location.lng;
+            this.message = `Location updated: ${new Date().getTime()}`;
+          });
+        }
+      );
     }
-
+  }
+  async initIOS() {
+    await CapacitorForegroundLocationService.initialize();
+    const response =
+      await CapacitorForegroundLocationService.requestPermission().catch(
+        (err: any) => {
+          console.log(`Error man ue: ` + err);
+          return {
+            granted: false,
+          } as PermissionResponse;
+        }
+      );
+    console.log(response);
+    if (response.granted) {
+      CapacitorForegroundLocationService.startUpdatingLocation();
+      CapacitorForegroundLocationService.addListener(
+        'locationUpdate',
+        (location: ForegroundLocation) => {
+          console.log('[JS] Lat:', location.lat, 'Lng:', location.lng);
+          this.ngZone.run(() => {
+            this.lat = location.lat;
+            this.lng = location.lng;
+            this.message = `Location updated: ${new Date().getTime()}`;
+          });
+        }
+      );
+    }
   }
 }
